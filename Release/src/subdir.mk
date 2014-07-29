e888d3b4a03db7fdb874d5345edaab8983bfa2fb
Shailen Sobhee <shailen@localhost.localdomain>
2014-07-30 00:15:29 +0200

    Adding base files and PCM library

---
+++ Release/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/RooflinePlotter.cpp 
+
+OBJS += \
+./src/RooflinePlotter.o 
+
+CPP_DEPS += \
+./src/RooflinePlotter.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Intel Intel(R) 64 C++ Compiler '
+	icpc -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/RooflinePlotter.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : RooflinePlotter.cpp
+// Author      : Shailen Sobhee
+// Version     :
+// Copyright   : OSS License.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "../lib/cpucounters.h"
+#include "../lib/utils.h"
+using namespace std;
+
+int main() {
+	PCM * m = PCM::getInstance();
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
